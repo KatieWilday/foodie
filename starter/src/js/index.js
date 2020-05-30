04e12aa6d78aea67a973a7e10b450e48cc931012
Katie Wilday <wildayk@gmail.com>
2020-05-30 10:37:15 -0400

    control search function

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,3 +1,33 @@
 import Search from './models/Search'
+/** Global State of the App
+* - Search object
+* - Current Recipe object
+* - Shopping List Oject
+* - Liked Recipes
+**/
+const state = {}
 
-const search = new Search('pizza')
+const controlSearch = async () => {
+  // 1. Get query from view
+  const query = 'pizza' //TODO
+
+  if (query) {
+    // 2. Create new search object add to state
+    state.search = new Search(query)
+
+    //3. Prepare UI for results
+
+    //4. Search for recipes
+    await state.search.getResults() //define as async to use await
+
+    //5. Render results on UI
+    console.log(state.search.result)
+  }
+
+}
+
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault()
+  controlSearch()
+})
--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
     try {
         const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`)
         this.result = res.data.recipes
-        console.log(recipes)
+        //console.log(this.result)
     } catch(error){
         alert(error)
     }
