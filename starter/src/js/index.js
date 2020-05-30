0752d1573d808330d55dccf6c01cebb5f3af48eb
Katie Wilday <wildayk@gmail.com>
2020-05-30 11:09:47 -0400

    search view

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
 /** Global State of the App
 * - Search object
 * - Current Recipe object
@@ -9,25 +11,30 @@ const state = {}
 
 const controlSearch = async () => {
   // 1. Get query from view
-  const query = 'pizza' //TODO
+  const query = searchView.getInput();
+  console.log(query) //TODO
 
   if (query) {
     // 2. Create new search object add to state
     state.search = new Search(query)
 
     //3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     //4. Search for recipes
-    await state.search.getResults() //define as async to use await
+    await state.search.getResults(); //define as async to use await
 
     //5. Render results on UI
+    searchView.renderResults(state.search.result)
     console.log(state.search.result)
+
   }
 
 }
 
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault()
   controlSearch()
 })
---
+++ starter/src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+  searchInput: document.querySelector('.search__field'),
+  searchForm: document.querySelector('.search'),
+  searchResultList: document.querySelector('.results__list')
+}
