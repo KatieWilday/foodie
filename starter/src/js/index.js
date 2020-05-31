35d1a9478add13e67713bfad91ccef6e1e9e3fd1
Katie Wilday <wildayk@gmail.com>
2020-05-31 14:54:13 -0400

    servings counter

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -99,3 +99,19 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling Recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+      // Decreased button is clicked
+      if (state.recipe.servings > 1) {
+        state.recipe.updateServings('dec')
+        recipeView.updateServingsIngredients(state.recipe);
+      }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+      // Increase button is clicked
+      state.recipe.updateServings('inc')
+      recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe)
+})
