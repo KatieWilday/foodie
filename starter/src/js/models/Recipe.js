5f35bd5f026b56eaa1a61a593f38ee73ad510cfa
Katie Wilday <wildayk@gmail.com>
2020-05-31 14:31:50 -0400

    hight selection

--- starter/src/js/models/Recipe.js
+++ starter/src/js/models/Recipe.js
@@ -35,7 +35,7 @@ export default class Recipe {
   parseIngredients() {
     const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds']
     const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound']
-
+    const units = [...unitsShort, 'kg', 'g']
 
     const newIngredients = this.ingredients.map(el => {
       // 1. Uniform units
@@ -49,7 +49,7 @@ export default class Recipe {
 
       //3. Parse ing into count, unit and ingredient
       const arrIng = ingredient.split(' ');
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
