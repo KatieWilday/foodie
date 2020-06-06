d35317a1e15b309e98f6fc4205c4e972b862c103
Katie Wilday <wildayk@gmail.com>
2020-06-06 10:22:39 -0400

    css complete

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,1 +1,13 @@
-//This is where your API key and Proxy would go. 
+fetch("https://tasty.p.rapidapi.com/tags/list", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "tasty.p.rapidapi.com",
+		"x-rapidapi-key": "acd05c5711msha05183ddac026c8p169ec7jsn414fc820b394"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
